6f3c6dcf69813b9137dd925b65a81a4ba691dd1d
Chandresh Thakur <chandresht149@gmail.com>
2025-08-16 20:26:10 +0530

    Added a new agent with complete fix

--- pg_connect.ps1
+++
@@ -1,9 +0,0 @@
-# Load PostgreSQL environment variables
-$env:PGUSER = "postgres"
-$env:PGPASSWORD = "postgres"
-$env:PGHOST = "localhost"
-$env:PGPORT = "5432"
-$env:PGDATABASE = "relationship_memory"
-
-# Test the connection
-psql -c "\conninfo"
